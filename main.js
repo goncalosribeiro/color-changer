42d466b9cf51b65755162fddddaf04c57bb9ea3e
Goncalo Ribeiro <32962095+goncalosribeiro@users.noreply.github.com>
2020-06-18 14:04:31 +0100

    Update main.js

--- main.js
+++ main.js
@@ -15,3 +15,14 @@ menuToggler.addEventListener('click', function () {
   body.classList.toggle('open');
 });
 
+var el = document.getElementsByClassName("color");
+for (var i = 0; i < el.length; i++) {
+  el[i].onclick = changeColor;
+}
+
+function changeColor(e) {
+  // get the hex color
+  let hex = e.target.getAttribute("data-hex");
+  // set the hex color
+  document.querySelector(".cls-1").style.fill = hex;
+}
